e809a727e9b57fa2a83b7b26d7ccc7344ced4b38
남영환 <ssisksl77@itembay.com>
2019-08-06 17:06:17 +0900

    emit demo add

--- resources/public/nodejs/emit/app.js
+++ resources/public/nodejs/emit/app.js
@@ -1,12 +1,23 @@
 var Emitter = require('./emitter');
+var eventConfig = require('./config').events;
 
+// var emtr = new Emitter();
+// emtr.on('greet', function() {
+//   console.log('listener 1');
+// });
+//
+// emtr.on('greet', function() {
+//   console.log('listener 2');
+// })
+//
+// emtr.emit('greet');
+
+var Emitter = require('events');
 var emtr = new Emitter();
-emtr.on('greet', function() {
-  console.log('listener 1');
-});
-
-emtr.on('greet', function() {
-  console.log('listener 2');
+emtr.on(eventConfig.GREET, function() {
+  console.log('greet1');
 })
-
-emtr.emit('greet');
+emtr.on(eventConfig.GREET, function() {
+  console.log('greet2');
+})
+emtr.emit(eventConfig.GREET);
